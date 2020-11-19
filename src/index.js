4414942b1577965c4184f418ebece30448855fb7
Emily Aguirre <emgal.aguirre@gmail.com>
2020-11-19 14:41:48 -0500

    updated src file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//this is the module and the object
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
